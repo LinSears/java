4bea2d05d40e6086a17888e05ba7157599089286
vadimB <k.idzumi@mail.ru>
2019-12-13 21:18:44 +0300

    урок7

---
+++ lesson 7.1/src/com/company/BankomatImpl.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public interface BankomatImpl {
+    boolean checkPin(String cartID, String pin);
+    float getBalance();
+    void getCash(float cash);
+    void addCash(float cash);
+    void reject();
+
+}
